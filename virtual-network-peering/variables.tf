d7039b5b9049bcf02d632b9c19e6052dc4e489d8
Brent McConnell <bmcconnell@Brents-iMac.local>
2019-10-04 08:24:27 -0400

    changed README

--- virtual-network-peering/variables.tf
+++ virtual-network-peering/variables.tf
@@ -1,9 +1,3 @@
-variable "prefix" {
-  description = "The prefix used for all resources in this example"
-  default = "Peering"
-}
-
 variable "location" {
-  description = "The Azure location where all resources in this example should be created"
-  default = "eastus"
+  description = "The Azure Region where all resources should be created (ex. usgovvirginia, eastus, etc)"
 }
